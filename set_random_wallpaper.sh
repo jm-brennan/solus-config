8666859cc9b6fc4d2a109e2a6cd9c341a1d87213
Jacob Brennan <bocajnannerb@gmail.com>
2022-03-23 23:22:15 -0400

    more helpful

--- set_random_wallpaper.sh
+++ set_random_wallpaper.sh
@@ -15,4 +15,7 @@ printf "set wallpaper to $WALLPAPER at $(date)" >> "$LOG_PATH"
 
 gsettings set org.gnome.desktop.background picture-uri "file://$SCREENSAVER_DIR$WALLPAPER"
 gsettings set org.gnome.desktop.screensaver picture-uri "file://$SCREENSAVER_DIR$WALLPAPER"
+# Normally, you would need to write to the /etc/lightdm/slick-greeter.conf file, but I wanted to 
+# not have to run this script as sudo, so I made a symlink from /etc/lightdm/slick-greeter.conf to 
+# a file in my home directory
 printf "[Greeter]\nbackground=$SCREENSAVER_DIR$WALLPAPER" > "/home/$USERNAME/solus/slick-greeter.conf"
